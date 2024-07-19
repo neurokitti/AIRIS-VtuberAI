9e66dac0577a000341cb6e1447940294138d63e3
neurokitti <168581144+neurokitti@users.noreply.github.com>
2024-07-19 13:41:44 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from startup_scripts import main_chat, main_interview
+
+if __name__ == "__main__":
+    main_chat() #this will run a chat mode that will interact with the chat but will not respond to you
+    #main_interview() # this will not read chat but instead respond to anyone on the stream over mic

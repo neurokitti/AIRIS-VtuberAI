0fcd03d87e441c96a0120a512e7a44a3126c9bdf
neurokitti <168581144+neurokitti@users.noreply.github.com>
2024-07-27 13:20:44 -0500

    added the twitch script runner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from startup_scripts import main_chat, main_interview
+from startup_scripts import main_chat, main_interview, main_chat_twitch
 
 if __name__ == "__main__":
-    main_chat() #this will run a chat mode that will interact with the chat but will not respond to you
+    #main_chat() #this will run a chat mode that will interact with the chat but will not respond to you
     #main_interview() # this will not read chat but instead respond to anyone on the stream over mic
+    main_chat_twitch()

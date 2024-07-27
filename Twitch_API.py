c0f2621e196d01ddc86b17157a2a7400285d36c4
neurokitti <168581144+neurokitti@users.noreply.github.com>
2024-07-27 13:21:32 -0500

    Add files via upload

---
+++ Twitch_API.py
@@ -0,0 +1,22 @@
+# Importing necessary modules from twitchio
+from twitchio.ext import commands
+
+# Define the bot class that inherits from commands.Bot
+class Bot(commands.Bot):
+
+    def __init__(self):
+        # Initialize the bot with token and channels
+        super().__init__(token='oauth:zaco5qix5486kmjvswipun1wauq483', prefix='!', initial_channels=['neuro_kitti'])
+        self.messages = []
+    # Event handler for when the bot is ready and connected
+    async def event_ready(self):
+        print(f'Logged in as {self.nick}')
+        print(f'User id is {self.user_id}')
+
+    # Event handler for when a message is sent in chat
+    async def event_message(self, message):
+        # Print the message content and the author to the console
+        dict_messge = {'role':'user', 'name':message.author.name, 'text':message.content}
+        self.messages.append(dict_messge)
+    def get_messages(self):
+        return self.messages
